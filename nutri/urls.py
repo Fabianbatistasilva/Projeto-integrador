163cbeb3ac418d935508554f29243e58be2a153f
fabianbatistasilva <currifabianbatista@gmail.com>
2022-05-24 12:25:11 -0300

    levando para senac

--- nutri/urls.py
+++ nutri/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns =[
     path('',views.index,name='home'),
     path('login/',views.UserLogin, name='login'),
-    path('introduçâo/',views.introducao, name='introdução'),
+    path('introducao/',views.introducao, name='introducao'),
 ]
